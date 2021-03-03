bc81244687fdbaccc2c02e334bce94d4a1b9538d
ali <alihossainrajubd@gmail.com>
2021-03-03 14:07:16 +0600

    Add project files.

---
+++ RoleForCore/Models/CrudInCoreContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace RoleForCore.Models
+{
+    public class CrudInCoreContext: IdentityDbContext
+    {
+        public CrudInCoreContext(DbContextOptions<CrudInCoreContext> options) : base(options)
+        {
+
+        }
+    }
+}
